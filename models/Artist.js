341c154512450fbdbe509fa3ca712cda3fe0e824
Thijs van Marle <thijsvanmarle@gmail.com>
2023-04-05 12:38:49 +0200

    Added models

---
+++ models/Artist.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectID = mongoose.Schema.Types.ObjectId;
+
+const ArtistSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    shortDescription: {
+        type: String,
+        required: true
+    },
+    urlName: {
+        type: String,
+        required: true
+    },
+    pressPic: {
+        type: String,
+        required: true
+    },
+    spotifyPlaylist: {
+        type: String,
+        required: true
+    },
+    youtubeLink: {
+        type: String,
+        required: true
+    },
+    instagramLink: {
+        type: String,
+        required: true
+    },
+    facebookLink: {
+        type: String,
+        required: true
+    },
+    driveLink: {
+        type: String,
+        required: true
+    },
+    shows: [{
+        type: ObjectID,
+        ref: 'Show'
+    }]
+})
+
+const Artist = mongoose.model('Artist', ArtistSchema)
+
+module.exports = Artist;
---
+++ models/Show.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectID = mongoose.Schema.Types.ObjectId;
+
+const ShowSchema = new Schema({
+    venue: {
+        type: String,
+        required: true
+    },
+    date:{
+        type: Date,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    artistID: {
+        type: ObjectID,
+        ref: 'Artist',
+        required: true
+    },
+    ticketLink: {
+        type: String,
+        required: true
+    },
+    expired: {
+        type: Boolean,
+        required: false
+    }
+})
+
+const Show = mongoose.model('Show', ShowSchema)
+
+module.exports = Show;
